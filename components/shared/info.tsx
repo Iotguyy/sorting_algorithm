b16bf9ba5ccab6eeb3c7581f7b9547d0f33e6e45
Iotguyy <163031531+Iotguyy@users.noreply.github.com>
2024-04-28 19:18:30 +0000

    removed code

--- components/shared/info.tsx
+++ components/shared/info.tsx
@@ -16,23 +16,23 @@ const Info: React.FC<InfoProps> = ({
   codeTS,
 }) => {
   return (
-    // <section className="flex flex-col gap-y-4">
-    //   {/* header */}
-    //   <h1 className="relative bottom-4 text-4xl font-black">{heading}</h1>
-    //   {/* TODO: description */}
+    <section className="flex flex-col gap-y-4">
+      {/* header */}
+      {/* <h1 className="relative bottom-4 text-4xl font-black">{heading}</h1> */}
+      {/* TODO: description */}
 
-    //   {/* (pseudo)code */}
-    //   <div className="flex flex-col gap-y-4 lg:flex-row lg:justify-between w-full">
-    //     <WindowSimulator
-    //       heading="Code"
-    //       codePY={codePY}
-    //       codeJS={codeJS}
-    //       codeTS={codeTS}
-    //     />
-    //     <WindowSimulator heading="Pseudocode" pseudocode={pseudocode} />
-    //   </div>
+      {/* (pseudo)code */}
+      {/* <div className="flex flex-col gap-y-4 lg:flex-row lg:justify-between w-full">
+        <WindowSimulator
+          heading="Code"
+          codePY={codePY}
+          codeJS={codeJS}
+          codeTS={codeTS}
+        />
+        <WindowSimulator heading="Pseudocode" pseudocode={pseudocode} />
+      </div> */}
 
-    //   {/* TODO: facts */}
+       {/* TODO: facts */}
     </section>
   );
 };
